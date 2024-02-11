0752e92a37c07c1a03551064f7cdeacead74dc60
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2024-02-11 20:58:52 +0530

    Changing content, adding favicon, responsiveness, dynamic age

---
+++ script.js
@@ -0,0 +1,36 @@
+function getBirtdate(){
+    const targetDate = new Date('2003-01-05');
+    const currentDate = new Date();
+
+    let ageInMillis = currentDate - targetDate;
+
+    let ageInYears = Math.floor(ageInMillis / (1000 * 60 * 60 * 24 * 365));
+    ageInMillis -= ageInYears * 1000 * 60 * 60 * 24 * 365;
+
+    let ageInMonths = Math.floor(ageInMillis / (1000 * 60 * 60 * 24 * 30.4375));
+    ageInMillis -= ageInMonths * 1000 * 60 * 60 * 24 * 30.4375;
+
+    let ageInDays = Math.floor(ageInMillis / (1000 * 60 * 60 * 24));
+    ageInMillis -= ageInDays * 1000 * 60 * 60 * 24;
+
+    let ageInHours = Math.floor(ageInMillis / (1000 * 60 * 60));
+    ageInMillis -= ageInHours * 1000 * 60 * 60;
+
+    let ageInMinutes = Math.floor(ageInMillis / (1000 * 60));
+    ageInMillis -= ageInMinutes * 1000 * 60;
+
+    let ageInSeconds = Math.floor(ageInMillis / 1000);
+
+    let age = `${ageInYears}y ${ageInMonths}m ${ageInDays}d ${ageInHours}:${ageInMinutes}:${ageInSeconds}s old`;
+
+    const ageDOM = document.getElementById("age")
+    ageDOM.textContent = age;
+    
+    return 1;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    getBirtdate()
+    setInterval(getBirtdate, 1000);
+
+});
