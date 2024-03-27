dd492c4435202f2dc4e51f07517ee8e3310d5bd1
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2024-03-27 13:45:08 +0530

    Fixing all like system bugs

--- blogs/script.js
+++ blogs/script.js
@@ -40,113 +40,119 @@ function getBlogPostLikes() {
 }
 
 function renderBlogPost() {
+  return new Promise((resolve, reject) => {
     const blogPostElement = document.getElementById('blog-post');
     const urlParams = new URLSearchParams(window.location.search);
     const markdownFile = urlParams.get('file');
 
     if (markdownFile) {
-        fetch(markdownFile)
-            .then(response => response.text())
-            .then(markdown => {
-                const renderer = new marked.Renderer();
-                let firstH1 = true;
-                let firstH2 = true;
+      fetch(markdownFile)
+        .then(response => response.text())
+        .then(markdown => {
+          const renderer = new marked.Renderer();
+          let firstH1 = true;
+          let firstH2 = true;
 
-                renderer.heading = function(text, level) {
-                    if (level === 1 && firstH1) {
-                        firstH1 = false;
+          renderer.heading = function (text, level) {
+            if (level === 1 && firstH1) {
+              firstH1 = false;
 
-                        const ogTitleElement = document.querySelector('meta[property="og:title"]');
-                        if (ogTitleElement) {
-                            ogTitleElement.setAttribute('content', text);
-                        };
-                        document.title = text
-                        return `<h1 id="blog-title">${text}</h1>`;
-                    } else if (level === 2 && firstH2) {
-                        firstH2 = false;
-                        return `<div class="blog-header"><h2 id="blog-date">${text}</h2><div id="likes" class="likes"><i id="like" class="fa-solid fa-heart"></i><span style="font-size: 16px;">0</span></div></div>`;
-                    }
-                    return `<h${level}>${text}</h${level}>`;
-                };
+              const ogTitleElement = document.querySelector('meta[property="og:title"]');
+              if (ogTitleElement) {
+                ogTitleElement.setAttribute('content', text);
+              };
+              document.title = text
+              return `<h1 id="blog-title">${text}</h1>`;
+            } else if (level === 2 && firstH2) {
+              firstH2 = false;
+              return `<div class="blog-header"><h2 id="blog-date">${text}</h2><div id="likes" class="likes"><i id="like" class="fa-solid fa-heart"></i><span style="font-size: 16px;">0</span></div></div>`;
+            }
+            return `<h${level}>${text}</h${level}>`;
+          };
 
-                renderer.link = function(href, title, text) {
-                    const target = '_blank';
-                    const titleAttr = title ? ` title="${title}"` : '';
-                    return `<a href="${href}"${titleAttr} target="${target}">${text}</a>`;
-                  };
+          renderer.link = function (href, title, text) {
+            const target = '_blank';
+            const titleAttr = title ? ` title="${title}"` : '';
+            return `<a href="${href}"${titleAttr} target="${target}">${text}</a>`;
+          };
 
-                const html = marked(markdown, { renderer });
-                blogPostElement.innerHTML = html;
-            })
-            .catch(error => {
-                console.error('Error fetching markdown file:', error);
-            });
+          const html = marked(markdown, { renderer });
+          blogPostElement.innerHTML = html;
+          resolve();
+        })
+        .catch(error => {
+          console.error('Error fetching markdown file:', error);
+          reject(error);
+        });
+    } else {
+      resolve();
     }
+  });
 }
 
 function loadPosts() {
-    const postsFile = 'posts.json';
-    const postsDiv = document.querySelector('.posts');
-  
-    fetch(postsFile)
-      .then(response => response.json())
-      .then(markdownFiles => {
-        Promise.all(markdownFiles.map(file => fetch(file).then(response => response.text())))
-          .then(markdownContents => {
-                    const posts = markdownContents.map((markdown, index) => {
-                        const renderer = new marked.Renderer();
-                        let title = '';
-                        let date = '';
+  const postsFile = 'posts.json';
+  const postsDiv = document.querySelector('.posts');
 
-                        renderer.heading = function(text, level) {
-                            if (level === 1 && !title) {
-                                title = text;
-                            } else if (level === 2 && !date) {
-                                date = text;
-                            }
-                            return '';
-                        };
+  fetch(postsFile)
+    .then(response => response.json())
+    .then(markdownFiles => {
+      Promise.all(markdownFiles.map(file => fetch(file).then(response => response.text())))
+        .then(markdownContents => {
+          const posts = markdownContents.map((markdown, index) => {
+            const renderer = new marked.Renderer();
+            let title = '';
+            let date = '';
 
-                        marked(markdown, { renderer });
-                        const fileName = markdownFiles[index].split('/').pop();
+            renderer.heading = function (text, level) {
+              if (level === 1 && !title) {
+                title = text;
+              } else if (level === 2 && !date) {
+                date = text;
+              }
+              return '';
+            };
 
-                        return {
-                            title: title,
-                            date: date,
-                            url: `posts/${fileName}`
-                        };
-                    });
+            marked(markdown, { renderer });
+            const fileName = markdownFiles[index].split('/').pop();
 
-                    displayPosts(posts);
-                });
+            return {
+              title: title,
+              date: date,
+              url: `posts/${fileName}`
+            };
+          });
+
+          displayPosts(posts);
         });
-}
-
-function displayPosts(posts) {
-    const postsDiv = document.querySelector('.posts');
-    postsDiv.innerHTML = '';
-
-    posts.forEach(post => {
-        const postElement = document.createElement('div');
-        postElement.classList.add('post');
-        postElement.setAttribute('onclick', `openBlog(this, '${post.url}')`);
-
-        const titleElement = document.createElement('a');
-        titleElement.classList.add('blog-title');
-        titleElement.textContent = post.title;
-        
-        const dateElement = document.createElement('div');
-        dateElement.classList.add('blog-date');
-        dateElement.textContent = post.date;
-
-        postElement.appendChild(titleElement);
-        postElement.appendChild(dateElement);
-        postsDiv.appendChild(postElement);
     });
 }
 
+function displayPosts(posts) {
+  const postsDiv = document.querySelector('.posts');
+  postsDiv.innerHTML = '';
+
+  posts.forEach(post => {
+    const postElement = document.createElement('div');
+    postElement.classList.add('post');
+    postElement.setAttribute('onclick', `openBlog(this, '${post.url}')`);
+
+    const titleElement = document.createElement('a');
+    titleElement.classList.add('blog-title');
+    titleElement.textContent = post.title;
+
+    const dateElement = document.createElement('div');
+    dateElement.classList.add('blog-date');
+    dateElement.textContent = post.date;
+
+    postElement.appendChild(titleElement);
+    postElement.appendChild(dateElement);
+    postsDiv.appendChild(postElement);
+  });
+}
+
 function openBlog(element, link) {
-    window.location.href = `blog.html?file=${link}`;
+  window.location.href = `blog.html?file=${link}`;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
